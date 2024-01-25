5ebda6febf3a835be3d514b29b57a2e2db2b9fc4
Sasha <georgefbrockman@gmail.com>
2024-01-25 11:49:12 -0600

    finishes lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    sequence = []
+    for n in range(length) :
+        if len(sequence) < 1 :
+            sequence.append(0)
+        elif len(sequence) < 2 :
+            sequence.append(1)
+        else :
+            sequence.append(sequence[n-1] + sequence[n-2])
+    print(sequence)
